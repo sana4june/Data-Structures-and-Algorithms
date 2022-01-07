e2907960d11df27a57451a6e5749610dec01a7c5
sana4june <33114744+sana4june@users.noreply.github.com>
2022-01-07 16:49:23 +0000

    Update L_7. Reverse Integer.java

--- Leetcode/Math/L_7. Reverse Integer.java
+++ Leetcode/Math/L_7. Reverse Integer.java
@@ -3,8 +3,7 @@ class Solution {
     public int reverse(int x) {
         int sign=1;
         int rev=0;
-        int num=x;
-        
+        int num=x;       
         
         if(x<0)
         {
@@ -17,14 +16,9 @@ class Solution {
             if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
             if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
             rev=rev*10+pop;
-            x=x/10;
-            
-            
+            x=x/10;           
         }
        
         return rev*sign;
-        
-        
-        
     }
 }
